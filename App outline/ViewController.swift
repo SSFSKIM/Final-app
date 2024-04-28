abc1c8d94147dcce626c97ad9161d1aebe76f5ec
SSFSKIM <kimmi@ssfs.org>
2024-04-27 21:39:34 -0400

    hu

--- App outline/ViewController.swift
+++ App outline/ViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Outs: UIStackView!
+    @IBOutlet weak var Points: UITextField!
+    @IBOutlet weak var PositionType: UITextField!
+    @IBOutlet weak var StatType: UITextView!
+    @IBOutlet weak var Player1: UITextView!
+    @IBOutlet weak var Player2: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
