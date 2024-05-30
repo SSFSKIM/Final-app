123c4a7b564e917ac6b9c732443957d9541a099c
SSFSKIM <kimmi@ssfs.org>
2024-05-30 11:15:10 -0400

    ㄴ

--- App outline/ViewController.swift
+++ App outline/ViewController.swift
@@ -4,22 +4,81 @@
 //
 //  Created by Steve on 4/27/24.
 //
-
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var Outs: UIStackView!
     @IBOutlet weak var Points: UITextField!
     @IBOutlet weak var PositionType: UITextField!
     @IBOutlet weak var StatType: UITextView!
-    @IBOutlet weak var Player1: UITextView!
-    @IBOutlet weak var Player2: UITextView!
+    @IBOutlet weak var Choice1: UITextView!
+    @IBOutlet weak var Choice2: UITextView!
+    
+    var hitters: [Hitter] = []
+    var pitchers: [Pitcher] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func loadRandomPlayers() {
+         let isPitcher = Bool.random()
+         if isPitcher {
+             PositionType.text = "Pitcher"
+             let player1 = pitchers.randomElement()!
+             let player2 = pitchers.randomElement()!
+             Choice1.text = "\(player1.playerName) \(player1.season)"
+             Choice2.text = "\(player2.playerName) \(player2.season)"
+             StatType.text = "WAR"
+         } else {
+             PositionType.text = "Hitter"
+             let player1 = hitters.randomElement()!
+             let player2 = hitters.randomElement()!
+             Choice1.text = "\(player1.playerName) \(player1.season)"
+             Choice2.text = "\(player2.playerName) \(player2.season)"
+             StatType.text = "WAR"
+         }
+     }
+
+     // Implement logic for checking the user's choice here
+ }
+
+    func loadHitterData() -> [Hitter] {
+        guard let path = Bundle.main.path(forResource: "hitdata", ofType: "json") else { fatalError("Couldn't find pitcherdata.json") }
+        let url = URL(fileURLWithPath: path)
+        
+        do {
+            let data = try Data(contentsOf: url)
+            let hitters = try JSONDecoder().decode([Hitter].self, from: data)
+            return hitters
+        } catch {
+            print("Error loading hitter data: \(error)")
+            return []
+        }
+    }
 
 
-}
+
+
+
+    func loadPitcherData() -> [Pitcher] {
+        guard let path = Bundle.main.path(forResource: "pitchdata", ofType: "json") else { fatalError("Couldn't find pitcherdata.json") }
+        let url = URL(fileURLWithPath: path)
+        
+        do {
+            let data = try Data(contentsOf: url)
+            let pitchers = try JSONDecoder().decode([Pitcher].self, from: data)
+            return pitchers
+        } catch {
+            print("Error loading pitcher data: \(error)")
+            return []
+        }
+        
+        
+    }
+
+   
+        
 
--- App outline/playerstruct.swift
+++ App outline/playerstruct.swift
@@ -14,7 +14,28 @@
 
 import Foundation
 
-struct Player: Codable {
+struct Hitter: Codable {
+    let playerName: String
+    let teamName: String
+    let season: Int
+    let age: Int
+    let AVG: Double
+    let BBPercent: Double
+    let KPercent: Double
+    let OBP: Double
+    let OPS: Double
+    let SLG: Double
+    let wRCPlus: Int
+    let wOBA: Double
+    let WAR: Double
+    let Defense: Double
+    let maxEV: Double
+    let HardHitPercent: Double
+    let BarrelPercent: Double
+    let EV: Double
+}
+
+struct Pitcher: Codable {
     let season: Int
     let age: Int
     let playerName: String
@@ -57,89 +78,4 @@ struct Player: Codable {
     let HardHit: Double
     let EV: Double
     let pfxvFA: Double
-
-    enum CodingKeys: String, CodingKey {
-        case season = "Season"
-        case age = "Age"
-        case playerName = "PlayerName"
-        case teamName = "TeamName"
-        case W
-        case L
-        case ERA
-        case QS
-        case IP
-        case H
-        case R
-        case ER
-        case HR
-        case BB
-        case HBP
-        case SO
-        case K9 = "K/9"
-        case BB9 = "BB/9"
-        case KBB = "K/BB"
-        case H9 = "H/9"
-        case AVG
-        case WHIP
-        case FIP
-        case WAR
-        case wFB
-        case wSL
-        case wCT
-        case wCB
-        case wCH
-        case wSF
-        case wKN
-        case wFBC = "wFB/C"
-        case wSLC = "wSL/C"
-        case wCTC = "wCT/C"
-        case wCBC = "wCB/C"
-        case wCHC = "wCH/C"
-        case wSFC = "wSF/C"
-        case wKNC = "wKN/C"
-        case Barrel
-        case HardHit
-        case EV
-        case pfxvFA = "pfxvFA"
-    }
 }
-
-struct PlayerData: Codable {
-    let data: [Player]
-    let totalCount: Int
-    let dateRange: String
-    let dateRangeSeason: String
-    let sortStat: String
-    let sortDir: String
-}
-func readJSONFromFile(fileName: String) -> Data? {
-    if let path = Bundle.main.path(forResource: fileName, ofType: "json") {
-        do {
-            let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
-            return data
-        } catch {
-            print("Error reading JSON file: \(error)")
-        }
-    }
-    return nil
-}
-func parsePlayerData() {
-    guard let jsonData = readJSONFromFile(fileName: "pitchdata") else {
-        print("Failed to read JSON file")
-        return
-    }
-
-    let decoder = JSONDecoder()
-    do {
-        let playerData = try decoder.decode(PlayerData.self, from: jsonData)
-        let players = playerData.data
-        // Now you have an array of Player objects
-        for player in players {
-            print("Player Name: \(player.playerName), Team: \(player.teamName), ERA: \(player.ERA)")
-        }
-    } catch {
-        print("Error decoding JSON: \(error)")
-    }
-}
-
-
